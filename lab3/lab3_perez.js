aeab11a373ae4801c0364151a00ee7e820577672
uhohdontdothat <henry256388792@gmail.com>
2025-02-06 15:21:09 -0500

    Update lab3_perez.js

--- lab3/lab3_perez.js
+++ lab3/lab3_perez.js
@@ -67,3 +67,66 @@ for(let index=0; index<num.length;index++){
 }
 console.log(`Sum of all negative numbers = ${sumneg}.`)
 console.log(`Sum of all positive numbers = ${sumpos}.`)
+
+console.log("\n------ Example 6: while loop as a counter ------")
+// use a while loop to display number from 0 to 4 --> 0 1 2 3 4
+let y = 0;
+while(y<=4){
+    console.log(y);
+    y++
+}
+
+console.log("\n------ Example 7: while loop application ------")
+// using while loop to check if the user number matches the secret number
+const SECRET = 8
+// collect a number from the user
+let usernumber = parseInt(prompt("Enter a number:"))
+let guesscounter = 0;
+// run a while to recollect the user number if the number doesn't match the secret number
+while(usernumber!==SECRET){
+    guesscounter++
+    usernumber = parseInt(prompt("Wrong guess! Enter another number:"))
+}
+console.log(`${usernumber} is right! Total attempts: ${guesscounter}`)
+
+console.log("\n------ Example 8: break in a while loop ------")
+// create an app to sum all positive even numbers. The app will continuosly collect a positive number and stop if a negative number is entered
+let sumeven = 0;
+let collectnumber;
+while (true){
+    collectnumber = parseInt(prompt("Enter a positive number:"))
+    if(collectnumber<0){
+        break
+    }
+    else{
+        if (collectnumber%2===0){
+            sumeven+=collectnumber
+        }
+    }
+}
+console.log(`The sum of all even numbers is ${sumeven}`)
+console.log("\n------ Example 9: continue in a for loop ------")
+// print number from -5 to 5, -5 -4 -3 -2 -1 0 1 2 3 4 5
+for(let n=-5;n<=5; n++){
+    if(n%2===0){
+        continue
+    }
+    console.log(n)
+}
+
+console.log("\n------ EXERCISE B ------")
+let secretpin = "0714";
+let guessattempts = 0
+let userpininput = prompt("Enter your 4 digit pin!: ")
+guessattempts++
+while (userpininput!==secretpin){
+    guessattempts++
+    userpininput = prompt("That wasn't the right pin! Please try again!")
+    if(guessattempts===3){
+        console.log("Account is locked!")
+        break
+    }
+}
+if(userpininput===secretpin){
+    console.log("Your pin number is correct!")
+}
