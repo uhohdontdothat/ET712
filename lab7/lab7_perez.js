33fc82e3f16167f5e66f76d05e0433ce57b7b671
uhohdontdothat <henry256388792@gmail.com>
2025-03-11 16:25:39 -0400

    Update lab7_perez.js

--- lab7/lab7_perez.js
+++ lab7/lab7_perez.js
@@ -52,3 +52,77 @@ btncircle.addEventListener("click", function(){
 btnsquare.addEventListener("click", function(){
     shape.className = "square"
 })
+
+/**
+ * Tuesday, March 11th, Traditional Events
+ */
+// Example 6
+// onmouseout event
+// collect the element
+let event6 = document.querySelector(".event6")
+// add a mouseout event to open an alert dialog box
+event6.onmouseout = function(){
+    alert("MOUSE OUT - Example 6")
+}
+
+// Example 7
+// Inline event
+function openalert(){
+    alert("CLICK EVENT - Example 7")
+}
+
+// example 8 mouseover event
+// change the background color of a div when the mouse hovers the element
+// collect element
+let divexample8 = document.querySelector(".divexample8")
+divexample8.onmouseout = function(){
+    let randcolorred = parseInt(Math.floor(Math.random()*255))
+    let randcolorgreen = parseInt(Math.floor(Math.random()*255))
+    let randcolorblue = parseInt(Math.floor(Math.random()*255))
+
+    divexample8.style.backgroundColor = `rgb(${randcolorred},${randcolorgreen},${randcolorblue})`
+}
+
+// Example 9: Keyboard Event
+// Collect the elements
+let inputexample9 = document.querySelector(".inputexample9")
+let example9_paragraph = document.querySelector(".example9_paragraph")
+
+inputexample9.onkeydown = function(event){
+    example9_paragraph.innerHTML = `key ${event.key} was pressed`
+}
+
+/**
+ * LAB EXERCISE
+ */
+// note: use querySelector() or querySelectorAll()
+// collect elements: search for the first p with class ps1
+let ps1 = document.querySelector(".ps1")
+// add a mouseout event to change the text content to "STUDENT'S FULL NAME"
+ps1.onmouseout = function(){
+    ps1.innerHTML = 'Henry Perez'
+}
+
+// search for the second p with class ps2
+// add a click event to the ps2 to change the font-size to 25px  and color to olive when is clicked
+let ps2 = document.querySelector(".ps2")
+ps2.addEventListener("click",function(){
+    ps2.style.fontSize = "25px"
+    ps2.style.color = "olive"
+})
+
+// search for the first div with class divs2
+// add a click event to change the background color to indigo
+let divs2 = document.querySelector(".divs2")
+divs2.addEventListener("click",function(){
+    divs2.style.backgroundColor = "indigo"
+})
+
+
+// search for the second div with class divs2 (did you mean divs3?)
+// add a keydown event to change the width of divs2 to 300px
+let divs3 = document.querySelector(".divs3")
+// discovered that looking for any input from the DOCUMENT makes the even trigger.
+document.onkeydown = function(){
+    divs3.style.width = "300px"
+}
